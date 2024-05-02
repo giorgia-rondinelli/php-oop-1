38b7b2edd418a6b0b744d6d68864906602e9f735
giorgia-rondinelli <g.rondinelli4@gmail.com>
2024-05-02 15:00:22 +0200

    definita classe, dichiarate istanze definito costruttore e funzione getInfo

--- Model/movie.php
+++ Model/movie.php
@@ -1,4 +1,24 @@
 <?php
 class Movie {
+  public $title;
+  public $year;
+  public $language;
+  public $genre;
+  public $director;
+
+  function __construct( string $_title, int $_year,$_director,string $_genre, string $_language)
+  {
+    $this->title = $_title;
+    $this->year = $_year;
+    $this-> director= $_director;
+    $this->genre= $_genre;
+    $this->language= $_language;
+
+  }
+
+  public function GetInfo(){
+    return "$this->title, $this->year, $this->director, $this->genre, $this->language";
+  }
   
+
 }
